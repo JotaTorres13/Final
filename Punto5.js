a0d9c9066810f1c984bdcb7d622b435566dfc7a1
JotaTorres13 <tecno.regionalmedellin@gmail.com>
2025-03-24 15:19:34 -0500

    Add Listar

--- Punto5.js
+++ Punto5.js
@@ -3,7 +3,6 @@ const prompt = require("prompt-sync")();
 let arrreglo = [                          // Definicion del arreglo principal
     {titulo:"", contenido:"", nota: 0},
 ]
-
 function crear() { // Función para crear una nota. 
     let titulo =prompt("Ingrese el titulo de la nota a ingresar: ")
     let contenido =prompt("Ingrese el contenido o detalle de la nota  : ")
@@ -14,7 +13,9 @@ function crear() { // Función para crear una nota.
             arrreglo.push({titulo,contenido,nota}) // al no ingresar al condicional se ingresa la nota al arreglo a traves del push. 
             console.log("Nota agregada satisfactoriamente")
 }
+
 // function editar(): { // Función para editar una nota. }
+
 function eliminar() { //Función para eliminar una nota.
     let titulo = prompt("Ingrese el titulo de la nota ha eliminar: "); //Solicita ingresar el titulo de la nota a eliminar.
     let index = arrreglo.findIndex(titulo=>arrreglo.titulo.toLowerCase()== titulo.toLowerCase()) //Recorre el arreglo hasta hallar el titulo de la nota a eliminar sin discriminar mayusculas gracias al TOLOWERCASE.
@@ -24,15 +25,28 @@ function eliminar() { //Función para eliminar una nota.
     }else{
         console.log("Estudiante no encontrado") }
 }   
-// function listar_notas():  { // Función para listar todas las notas. }
+function listar() { // Función para listar todas las notas. 
+    if(arrreglo.length===0){
+        console.log("No hay estudinates registrados")
+        return;
+    }
+    console.log("Lista de estudiantes");
+    arrreglo.forEach(arrreglo=>{
+        console.log(`-${arrreglo.titulo}: ${arrreglo.contenido}: ${arrreglo.nota}`)
+    })
+}
+    
 // function buscar_nota(): { //Función para buscar una nota por título. }
 
 flag= true // Inicializacion de una bandera.
 while(flag===true){ // Inicializacion de un ciclo, se ejecutara ya que va atado a la bandera declara inicialmente.
     console.log("\n Sistema de Gestion de Estudiantes") // Explicacion al usuario del menu por medio de pantalla.
-    console.log("4.")
+    console.log("1. Crear Nueva Nota")
+    console.log("2. ")
+    console.log("3. Eliminar Nota")
+    console.log("4. Listar Notas Registradas")
     console.log("5.")
-    console.log("6.Salir")
+    console.log("6. Salir")
 
     let opcion = prompt("Seleccione una opcion: ") // Opciones de usuario segun su eleccion se ejecutaran las funciones predeterminadas en el codigo.
 
@@ -45,19 +59,18 @@ while(flag===true){ // Inicializacion de un ciclo, se ejecutara ya que va atado 
             ;
             break;
         case "3":
-            eliminar()
-            ;
+            eliminar();
             break;
         case "4":
-            ;
+            listar();
             break;
         case "5":
             ;
-            break
+            break;
         case "6":
             console.log("Saliendo del sistema")
             flag === false
-            break
+            break;
         default:
             console.log("Opcion no valida")
     }
