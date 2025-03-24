7c98781172d2f887613905e5caa3bc7199e914e5
JotaTorres13 <tecno.regionalmedellin@gmail.com>
2025-03-24 14:43:59 -0500

    Add Eliminar Nota

--- Punto5.js
+++ Punto5.js
@@ -4,35 +4,39 @@ let arrreglo = [                          // Definicion del arreglo principal
     {titulo:"", contenido:"", nota: 0},
 ]
 
-function crear() {
+function crear() { // Función para crear una nota. 
     let titulo =prompt("Ingrese el titulo de la nota a ingresar: ")
     let contenido =prompt("Ingrese el contenido o detalle de la nota  : ")
     let nota = parseFloat(prompt(`Ingrese la calificacion de ${titulo}: `)) 
-        if(isNaN(nota) || nota<0 || nota>5 ){
+        if(isNaN(nota) || nota<0 || nota>5 ){ // condicional para manejar errores en las notas ingresadas. 
             console("Calificacion invalida")
             return; } 
-            arrreglo.push({titulo,contenido,nota})
+            arrreglo.push({titulo,contenido,nota}) // al no ingresar al condicional se ingresa la nota al arreglo a traves del push. 
             console.log("Nota agregada satisfactoriamente")
 }
-
-// function editar_nota(): { // Función para editar una nota. }
-// function eliminar_nota(): { //Función para eliminar una nota. }
+// function editar(): { // Función para editar una nota. }
+function eliminar() { //Función para eliminar una nota.
+    let titulo = prompt("Ingrese el titulo de la nota ha eliminar: "); //Solicita ingresar el titulo de la nota a eliminar.
+    let index = arrreglo.findIndex(titulo=>arrreglo.titulo.toLowerCase()== titulo.toLowerCase()) //Recorre el arreglo hasta hallar el titulo de la nota a eliminar sin discriminar mayusculas gracias al TOLOWERCASE.
+    if(index !== -1){
+        arrreglo.splice(index,1)
+        console.log("Nota eliminada")
+    }else{
+        console.log("Estudiante no encontrado") }
+}   
 // function listar_notas():  { // Función para listar todas las notas. }
 // function buscar_nota(): { //Función para buscar una nota por título. }
 
-flag= true
-while(flag===true){
-    console.log("\n Sistema de Gestion de Estudiantes")
-    console.log("1.Crear Nota")
-    console.log("2.")
-    console.log("3.")
+flag= true // Inicializacion de una bandera.
+while(flag===true){ // Inicializacion de un ciclo, se ejecutara ya que va atado a la bandera declara inicialmente.
+    console.log("\n Sistema de Gestion de Estudiantes") // Explicacion al usuario del menu por medio de pantalla.
     console.log("4.")
     console.log("5.")
     console.log("6.Salir")
 
-    let opcion = prompt("Seleccione una opcion: ")
+    let opcion = prompt("Seleccione una opcion: ") // Opciones de usuario segun su eleccion se ejecutaran las funciones predeterminadas en el codigo.
 
-    switch(opcion){
+    switch(opcion){ // Ciclo para iniciar la funcion seleccionada por el usuario.
 
         case "1":
             crear();
@@ -41,6 +45,7 @@ while(flag===true){
             ;
             break;
         case "3":
+            eliminar()
             ;
             break;
         case "4":
