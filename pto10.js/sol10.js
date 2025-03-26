da428f5916f135311ff7f0604a16c62c69470ec3
gbedoyacamilo <juan.bedoyag@comunidad,iush.edu.co>
2025-03-25 20:51:35 -0500

    punto 10

---
+++ pto10.js/sol10.js
@@ -0,0 +1,128 @@
+let cuentas = []
+function crearCuenta(nombre,saldoIn) {
+ 
+     if (isNaN(saldoIn || saldoIn < 0)){
+        console.log("Error, DATO NO VALIDO");
+        return;
+     }
+     const cuenta = {
+        nombre: nombre,
+        saldo: parseFloat(saldoIn)
+     };
+     cuentas.push(cuenta);
+     console.log(`cuenta creada con exito${nombre} y tu saldo es ${saldoIn}`);
+
+}
+function depositoDinero(nombre, cantidad) {
+
+     if(isNaN(cantidad)|| cantidad <= 0){
+        console.log(`Error. la cantidad no es valida`);
+        return;
+     }
+     const cuenta = cuentas.find(cuenta => cuenta.nombre === nombre);
+
+     if(cuenta){
+        cuenta.saldo += parseFloat(cantidad);
+        console.log(`se ha depositado ${cantidad} en la cuenta de ${nombre} nuevo saldo ${cuenta.saldo}`);
+     } else {
+        console.log(" no se encontro la cuenta");
+     }
+
+}
+function retiroDinero (nombre, cantidad){
+    if(isNaN(cantidad)|| cantidad <= 0){
+        console.log(`Error. la cantidad no es valida`);
+        return;
+     }
+     const cuenta = cuentas.find(cuenta => cuenta.nombre === nombre);
+
+     if(cuenta){
+        if(cuenta.saldo >= cantidad){
+            cuenta.saldo -= parseFloat(cantidad);
+            console.log(`se retiro ${cantidad} de cuenta ${nombre} nuevo saldo ${cuenta.saldo}`);
+        } else {
+            console.log("No se realizo con exito por falta de dinero ");
+        }
+     }
+}
+
+function consultarSaldo (nombre){
+    const cuenta = cuentas.find(cuenta => cuenta.nombre === nombre);
+
+       if (cuenta){
+        console.log(`Sldo de su cuenta ${nombre} es ${cuenta.saldo}`);
+       } else {
+        console.log("Error. cuenta no encontrada");
+       }
+}
+
+function listaCuentas (){
+    if (cuentas.length === 0) {
+        console.log ("No se ven cuentas creadas");
+        return;
+    }
+        console.log("cuenta creada con exito");
+        cuentas.forEach(cuenta => {
+            console.log(`${cuenta.nombre} ${cuenta.saldo}`);
+        });
+
+}
+function menu (){
+   let banco = false;
+
+   while(banco){
+    console.log("\n Menú Banco ");
+    console.log("1. Crear cuenta");
+    console.log("2. Depositar dinero");
+    console.log("3. Retirar dinero");
+    console.log("4. Consultar saldo");
+    console.log("5. Listar cuentas");
+    console.log("6. Salir");
+
+    let opcion = prompt("elegir una opcion entre 1-6");
+
+      switch (opcion){
+        case 1:  
+        const nombreCuenta = prompt("Ingrese el nombre para la nueva cuenta: ");
+        const saldoIn = parseFloat(prompt("Ingrese el saldo inicial de la cuenta: "));
+        crearCuenta(nombreCuenta, saldoIn);
+        break;
+    case 2:  
+        const nombreDeposito = prompt("Ingrese el nombre de la cuenta para depositar: ");
+        const cantidadDeposito = parseFloat(prompt("Ingrese la cantidad a depositar: "));
+        depositoDinero(nombreDeposito, cantidadDeposito);
+        break;
+    case 3:  
+        const nombreRetiro = prompt("Ingrese el nombre de la cuenta para retirar: ");
+        const cantidadRetiro = parseFloat(prompt("Ingrese la cantidad a retirar: "));
+        retiroDinero(nombreRetiro, cantidadRetiro);
+        break;
+    case 4:  
+        const nombreConsulta = prompt("Ingrese el nombre de la cuenta para consultar el saldo: ");
+        consultarSaldo(nombreConsulta);
+        break;
+    case 5:  
+        listaCuentas();
+        break;
+    case 6:  
+        console.log("¡Adios!");
+        salir = true;  
+        break;
+    default:
+        console.log("Opción inválida. ");
+        break;
+}
+}
+}
+
+
+menu();
+
+      
+
+
+
+
+
+
+
